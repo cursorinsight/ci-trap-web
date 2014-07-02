bab1b9fccd1cbad9a33b42c7660cc43fd239f72b
Golda Bence <gbence@algernon.hu>
2014-07-02 19:49:47 +0200

    Fix deprecation warnings in 'serve' task
    - Change gulp-connect to raw connect and co. due to deprecation warnings
      and hard-to-implement features in this ecosystem (vs. "a middleware
      that can handle a simple POST call without deprecation warnings").

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
+
+// default server port.
+var port = 8100;
+
+// processing task
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var connect = require('gulp-connect');
 
 gulp.task('scripts', function() {
   gulp.src(['algernon-trap.js', 'examples/app.js'])
@@ -15,30 +19,29 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./examples'))
 });
 
+// server task
+var connect = require('connect');
+var http = require('http');
+var util = require('gulp-util');
+
 gulp.task('serve', ['scripts'], function () {
-  // var bodyParser = require('body-parser');
-  connect
-    .server({
-      root: './examples',
-      port: 8100,
-      middleware: function(connect, opt) {
-        return [
-          connect.bodyParser(),
-          function (req, res, next) {
-            if (req.method == 'POST') { // && req.body['motion-data']) {
+  var app = connect()
+              .use(connect.urlencoded())
+              .use(function (req, res, next) {
+                if (req.method == 'POST') { // && req.body['motion-data']() {}
+                  util.log('Motion data received (' + req.body['motion-data'].length + ' bytes):\n'
+                         + '-----BEGIN MOTION DATA-----\n'
+                         + req.body['motion-data'] + '\n'
+                         + '-----END MOTION DATA-----');
+                  res.end('ok');
+                } else {
+                  next();
+                }
+              })
+              .use(connect.static('./examples'));
 
-              console.log('Motion data received (' + req.body['motion-data'].length + ' bytes):')
-              console.log('-----BEGIN MOTION DATA-----');
-              console.log(req.body['motion-data']);
-              console.log('-----END MOTION DATA-----');
-
-              res.end('ok');
-            } else {
-              next();
-            }
-          }
-        ];
-      }
-    });
+  var server = http.createServer(app);
+  util.log(util.colors.green('Server started on http://localhost:'+port+'/'));
+  server.listen(port);
 });
 
