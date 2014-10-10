561ddea4d93d27482037fc2461870cb030068d7f
Golda Bence <gbence@algernon.hu>
2014-10-10 12:35:38 +0200

    Fix window position polling
    Window position polling fetched current timestamp (`getDt()`) every
    `longDelay` milliseconds that reset idle timers in `state.js`.
    Separating the polling part from handling window position change events
    solved this.

--- src/algernon-trap/windowPositionHandler.js
+++ src/algernon-trap/windowPositionHandler.js
@@ -1,10 +1,11 @@
 /* global module */
 
-var windowPositionHandler = function(window, state, buffer) {
+var WindowPositionHandler = function(window, state, buffer) {
 "use strict";
 // ---------------------------------------------------------------------------
 
 var
+  eventName = "positionchanged",
   longDelay = 1000 / 2, // 2fps
   shortDelay = 1000 / 15, // 15fps
   throttleBase = 15, // it's a "constant"
@@ -19,46 +20,62 @@ var
     return window.screenY || window.screenTop || 0;
   },
 
-  handler = function() {
+  handler = function(event) {
+    var dT = state.getDT(null, 20);
+
+    state.windowPositionX = event.x;
+    state.windowPositionY = event.y;
+
+    // type = 0b1001
+    buffer.push([9, dT, event.x, event.y],
+                [4, 20,      15,      15]);
+  },
+
+  poller = function() {
     var
       x = windowPositionX(),
-      y = windowPositionY(),
-      dT = state.getDT(null, 20);
+      y = windowPositionY();
 
     if (state.windowPositionX !== x || state.windowPositionY !== y) {
-
-      state.windowPositionX = x;
-      state.windowPositionY = y;
-
-      // type = 0b1001
-      buffer.push([9, dT,  x,  y],
-                  [4, 20, 15, 15]);
+      var event = new window.Event(eventName);
+      event.x = x;
+      event.y = y;
+      window.dispatchEvent(event);
 
       throttleCount = throttleBase;
     }
 
     if (throttleCount > 0) {
       throttleCount--;
-      timeout = window.setTimeout(handler, shortDelay);
+      timeout = window.setTimeout(poller, shortDelay);
     } else {
-      timeout = window.setTimeout(handler, longDelay);
+      timeout = window.setTimeout(poller, longDelay);
     }
+  },
 
+  startPoller = function() {
+    window.setTimeout(poller, longDelay);
+  },
+
+  stopPoller = function() {
+    if (timeout) {
+      window.clearTimeout(timeout);
+    }
   };
 
 this.start = function() {
   state.windowPositionX = windowPositionX();
   state.windowPositionY = windowPositionY();
-  handler();
+  window.addEventListener(eventName, handler, false);
+  startPoller();
 };
 
 this.stop = function() {
-  if (timeout) {
-    window.clearTimeout(timeout);
-  }
+  stopPoller();
+  window.removeEventListener(eventName, handler, false);
 };
 
 // ---------------------------------------------------------------------------
 };
 
-module.exports = windowPositionHandler;
+module.exports = WindowPositionHandler;
