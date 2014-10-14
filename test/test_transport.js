51ecd31f1c3a41a949936f13999f2548e8b4c0f0
Golda Bence <gbence@algernon.hu>
2014-10-14 16:54:02 +0200

    Fix tests
    - Change transport.buffer -> transport.buffer() notation.
    - Add pushRawBytes() test.

--- test/test_transport.js
+++ test/test_transport.js
@@ -10,46 +10,46 @@ describe("AlgernonTrap::Transport", function() {
 
   var transport = new Transport(window);
 
-  describe("buffer", function() {
-
-    beforeEach(function() {
-      transport.reset();
-    });
-
-    // 0 0 ++++ -> 000000 (A)
-    it("should serialize [0, 0], [1, 1] to 'A'", function() {
-      transport.push([0, 0], [1, 1]);
-      assert.equal(transport.buffer, "A");
-    });
-
-    // 0 010 ++ -> 001000 (I)
-    it("should serialize [0, 2], [1, 3] to 'I'", function() {
-      transport.push([0, 2], [1, 3]);
-      assert.equal(transport.buffer, "I");
-    });
-
-    // 010101 101010 -> 010101 101010 (Vq)
-    it("should serialize [0, 2], [6, 6] to 'Vq'", function() {
-      transport.push([21, 42], [6, 6]);
-      assert.equal(transport.buffer, "Vq");
-    });
-
-    // 0000001 +++++ -> 000000 100000 (Vq)
-    it("should serialize [1], [7] to 'Ag'", function() {
-      transport.push([1], [7]);
-      assert.equal(transport.buffer, "Ag");
-    });
-
-    it("should serialize underflow values to 0", function() {
-      transport.push([-1, -100], [6, 12]);
-      assert.equal(transport.buffer, "AAA");
-    });
-
-    it("should serialize overflow values to MAX_BITS", function() {
-      transport.push([1000000, 1000000], [6, 9]);
-      assert.equal(transport.buffer, "//4");
-    });
-
+  beforeEach(function() {
+    transport.reset();
   });
 
+  // 0 0 ++++ -> 000000 (A)
+  it("should serialize [0, 0], [1, 1] to 'A'", function() {
+    transport.push([0, 0], [1, 1]);
+    assert.equal(transport.buffer(), "A");
+  });
+
+  // 0 010 ++ -> 001000 (I)
+  it("should serialize [0, 2], [1, 3] to 'I'", function() {
+    transport.push([0, 2], [1, 3]);
+    assert.equal(transport.buffer(), "I");
+  });
+
+  // 010101 101010 -> 010101 101010 (Vq)
+  it("should serialize [0, 2], [6, 6] to 'Vq'", function() {
+    transport.push([21, 42], [6, 6]);
+    assert.equal(transport.buffer(), "Vq");
+  });
+
+  // 0000001 +++++ -> 000000 100000 (Vq)
+  it("should serialize [1], [7] to 'Ag'", function() {
+    transport.push([1], [7]);
+    assert.equal(transport.buffer(), "Ag");
+  });
+
+  it("should serialize underflow values to 0", function() {
+    transport.push([-1, -100], [6, 12]);
+    assert.equal(transport.buffer(), "AAA");
+  });
+
+  it("should serialize overflow values to MAX_BITS", function() {
+    transport.push([1000000, 1000000], [6, 9]);
+    assert.equal(transport.buffer(), "//4");
+  });
+
+  it("should serialize raw bytes", function() {
+    transport.pushRawBytes("hello, world!");
+    assert.equal(transport.buffer(), "ARhello%2C%20world!"); // AR == 17 bytes
+  });
 });
